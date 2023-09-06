352f25cc49172f54ad03cfcd1a42c522a2cb4e7f
caiomelotec <caio.melotec@icloud.com>
2023-09-07 01:00:16 +0200

    header pt/2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,70 @@
-// import { useState } from "react";
+import React, { useState } from "react";
 import "../styles/Header.css";
 import { BsFillCartFill } from "react-icons/bs";
 import { FiSearch } from "react-icons/fi";
-import { useQueryContext } from "../contex/QueryContext";
+import { useNavigate } from "react-router-dom";
+import books from "../data/fakebooks.json";
+import { useResults } from "../contex/ResultsContext";
+
+type Book = {
+  id: number;
+  title: string;
+  author: string;
+  price: number;
+};
 
 export const Header = () => {
-  const { query, setQuery } = useQueryContext();
+  const navigate = useNavigate();
+
+  const { setResults } = useResults();
+
+  const [query, setQuery] = useState<string>("");
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setQuery(e.target.value);
   };
 
+  /*SEARCH FUNCTION */
+  const filterBooks = (): void => {
+    const filteredResults: Book[] = books.filter(
+      (book) =>
+        book.title.toLowerCase().includes(query.toLowerCase()) ||
+        book.author.toLowerCase().includes(query.toLowerCase())
+    );
+
+    setResults(filteredResults);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    if (query === "") {
+      setResults([]);
+      return;
+    }
+
+    filterBooks();
+
+    navigate("/results");
+
+    setQuery("");
+  };
+
   return (
     <header>
-      <h1 className="h1-logo">BookStore</h1>
-      <form action="" className="form-search">
+      <h1 onClick={() => navigate("/")} className="h1-logo">
+        BookStore
+      </h1>
+      <form action="" className="form-search" onSubmit={handleSubmit}>
         <input
           onChange={handleInputChange}
           value={query}
           type="search"
           className="input-search"
         />
-        <FiSearch className="search-icon" size={35} />
+        <button className="search-icon" type="submit">
+          <FiSearch className="search-icon" size={35} />
+        </button>
       </form>
       <nav>
         <a href="">Home</a>
