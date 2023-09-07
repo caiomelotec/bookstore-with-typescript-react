16c66c7644b6b5ce285c7e7228f86a3bb05c026b
caiomelotec <caio.melotec@icloud.com>
2023-09-08 01:58:47 +0200

    add Home router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Header } from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import { Results } from "./pages/Results";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/results" element={<Results />} />
       </Routes>
     </>
