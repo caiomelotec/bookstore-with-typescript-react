f07aa845676ff68955d63fda372ee9b9cae03469
caiomelotec <caio.melotec@icloud.com>
2023-09-09 21:11:29 +0200

    add shopping provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ResultsProvider } from "./contex/ResultsContext.tsx";
+import { ShoppingCartProvider } from "./contex/ShoppingCartContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ResultsProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ResultsProvider>
+    <ShoppingCartProvider>
+      <ResultsProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ResultsProvider>
+    </ShoppingCartProvider>
   </React.StrictMode>
 );
