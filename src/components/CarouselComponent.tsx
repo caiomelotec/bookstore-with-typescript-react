17724836f07d4613295d441c5bd14172179dc0e1
caiomelotec <caio.melotec@icloud.com>
2023-09-09 22:03:37 +0200

    add a type to carousel

--- src/components/CarouselComponent.tsx
+++ src/components/CarouselComponent.tsx
@@ -1,17 +1,17 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import books from "../data/fakebooks.json";
+// import books from "../data/fakebooks.json";
 import "../styles/Carousel.css";
 
-interface CarouselComponentProps {
-  deviceType: string; // Change the type to an array of strings
-}
+// interface CarouselComponentProps {
+//   deviceType: string; // Change the type to an array of strings
+// }
 
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 5,
-    slidesToSlide: 3, // optional, default to 1.
+    items: 6,
+    slidesToSlide: 2, // optional, default to 1.
   },
   tablet: {
     breakpoint: { max: 1024, min: 825 },
@@ -29,8 +29,14 @@ const responsive = {
     slidesToSlide: 1, // optional, default to 1.
   },
 };
+interface CarouselComponentProps {
+  books: {
+    id: number;
+    imgUrl: string;
+  }[];
+}
 
-export const CarouselComponent = (props: CarouselComponentProps) => {
+export const CarouselComponent = ({ books }: CarouselComponentProps) => {
   return (
     <>
       <div className="test-01">
@@ -41,14 +47,14 @@ export const CarouselComponent = (props: CarouselComponentProps) => {
           responsive={responsive}
           ssr={true}
           infinite={true}
-          autoPlay={props.deviceType !== "mobile"}
+          // autoPlay={props.deviceType !== "mobile"}
           autoPlaySpeed={2000}
           keyBoardControl={true}
           customTransition="all 0.5s"
           transitionDuration={500}
           containerClass="carousel-container"
           removeArrowOnDeviceType={["tablet", "mobile", "test"]}
-          deviceType={props.deviceType}
+          // deviceType={props.deviceType}
           dotListClass="custom-dot-list-style"
           itemClass="carousel-item-padding-40-px"
         >
