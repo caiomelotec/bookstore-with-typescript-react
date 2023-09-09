644733ddcf41e00db925b4ed05365eb88198b4e8
caiomelotec <caio.melotec@icloud.com>
2023-09-09 22:06:18 +0200

    responsive nav bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import { FiSearch } from "react-icons/fi";
 import { useNavigate, Link } from "react-router-dom";
 import books from "../data/fakebooks.json";
 import { useResults } from "../contex/ResultsContext";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 type Book = {
   id: number;
@@ -19,6 +20,7 @@ export const Header = () => {
   const { setResults } = useResults();
 
   const [query, setQuery] = useState<string>("");
+  const [toggle, setToggle] = useState<boolean>(false);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setQuery(e.target.value);
@@ -48,33 +50,79 @@ export const Header = () => {
     navigate("/results");
 
     setQuery("");
+
+    if (toggle) {
+      setToggle(false);
+    }
   };
 
   return (
     <header>
-      <h1 onClick={() => navigate("/")} className="h1-logo">
-        BookStore
-      </h1>
-      <form action="" className="form-search" onSubmit={handleSubmit}>
-        <input
-          onChange={handleInputChange}
-          value={query}
-          type="search"
-          className="input-search"
-        />
-        <button className="search-icon" type="submit">
-          <FiSearch className="search-icon" size={35} />
-        </button>
-      </form>
+      <div className="logo-and-input-div">
+        <h1 onClick={() => navigate("/")} className="h1-logo">
+          BookStore
+        </h1>
+        <form action="" className="form-search" onSubmit={handleSubmit}>
+          <input
+            onChange={handleInputChange}
+            value={query}
+            type="search"
+            className="input-search"
+          />
+          <button className="search-icon" type="submit">
+            <FiSearch className="search-icon" size={35} />
+          </button>
+        </form>
+      </div>
       <nav>
-        <Link to="/">Home</Link>
-        <a href="">About</a>
-        <a href="">Store</a>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        <a className="nav-link">About</a>
+        <Link to="/store" className="nav-link">
+          Store
+        </Link>
       </nav>
       <div className="warenkorb">
-        <BsFillCartFill size={22} />
+        <BsFillCartFill className="warenkorb-icon" size={22} />
         <span>0</span>
       </div>
+      <button
+        className="hamburger-menu"
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        <GiHamburgerMenu size={30} fill="white" className="hamburger-menu" />
+      </button>
+      <nav
+        className="mobile-menu"
+        style={toggle ? { left: "0" } : { left: "-50rem" }}
+      >
+        <Link
+          onClick={() => {
+            setToggle(false);
+          }}
+          className="mobile-nav-link"
+          to="/"
+        >
+          Home
+        </Link>
+        <a className="mobile-nav-link">About</a>
+        <Link
+          className="mobile-nav-link"
+          onClick={() => {
+            setToggle(false);
+          }}
+          to="/store"
+        >
+          Store
+        </Link>
+        <div className="checkout-div">
+          <BsFillCartFill className="" size={25} />
+          <span> Press to Checkout</span>
+        </div>
+      </nav>
     </header>
   );
 };
