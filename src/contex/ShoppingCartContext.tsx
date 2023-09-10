4a090986c1e2de6b522106acca16ce232f031d68
caiomelotec <caio.melotec@icloud.com>
2023-09-10 11:13:37 +0200

    some comments

--- src/contex/ShoppingCartContext.tsx
+++ src/contex/ShoppingCartContext.tsx
@@ -1,43 +1,48 @@
 import { createContext, ReactNode, useContext } from "react";
 import { useLocalStorage } from "../hooks/useLocalSotrage";
-
+// Define the type for the props of ShoppingCartProvider
 type ShoppingCartProviderProps = {
   children: ReactNode;
 };
-
+// Define the structure of a CartItem
 type CartItem = {
   id: number;
   quantity: number;
 };
-
+// Define the structure of the ShoppingCartContext
 type ShoppingCartContext = {
-  getItemQuantity: (id: number) => number;
+  getItemQuantity: (id: number) => number; // Function to get item quantity by id
   increaseCartQuantity: (id: number) => void;
   decreaseCartQuantity: (id: number) => void;
   removeFromCart: (id: number) => void;
-  cartQuantity: number;
-  cartItems: CartItem[];
+  cartQuantity: number; // Total quantity of items in the cart
+  cartItems: CartItem[]; // Array of items in the cart
 };
-
+// Create a context for the shopping cart and provide an initial empty object
 const ShoppingCartContext = createContext({} as ShoppingCartContext);
 
+// Custom hook to access the shopping cart context
 export function useShoppingCart() {
   return useContext(ShoppingCartContext);
 }
+
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  // Initialize cartItems using local storage and an empty array
   const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
     "shopping-cart",
     []
   );
 
+  // Calculate the total quantity of items in the cart
   const cartQuantity = cartItems.reduce(
     (quantity, item) => item.quantity + quantity,
     0
   );
-
+  // Function to get the quantity of an item by its id
   function getItemQuantity(id: number) {
     return cartItems.find((item) => item.id === id)?.quantity || 0;
   }
+
   function increaseCartQuantity(id: number) {
     setCartItems((currItems) => {
       if (currItems.find((item) => item.id === id) == null) {
